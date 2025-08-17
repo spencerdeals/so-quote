12cbebd7eacd4071e1e85a2d55f10d9e4fbaa8a7
Richard Spencer <contact@spencerdeals.bm>
2025-08-17 07:35:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// serve files from "public" folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// debug route
 app.get("/debug-index", (req, res) => {
   res.type("text/plain").send("✅ Deployed build has /debug-index. v=1");
 });
 
-// health check
 app.get("/health", (req, res) => res.send("ok"));
 
 const PORT = process.env.PORT || 3000;
