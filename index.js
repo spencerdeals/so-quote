231d68772a9738281e7bb8f209b9dfe661fa6f72
Richard Spencer <contact@spencerdeals.bm>
2025-08-21 00:17:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ app.get("/meta", async (req, res) => {
   try {
     const { body } = await request(url, {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "text/html",
         "Accept-Language": "en-US,en;q=0.9"
       },
       maxRedirections: 5
